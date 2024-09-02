ff73ab0d292087dd430af7070c6a30784a8ffeb9
hades255 <hades.255@outlook.com>
2024-09-02 12:12:24 -0400

    update copy link with webapp

--- src/components/leaderbaord/CopyBtn.js
+++ src/components/leaderbaord/CopyBtn.js
@@ -9,7 +9,7 @@ const CopyBtn = () => {
   const dispatch = useDispatch();
 
   const inviteLink = useMemo(
-    () => `https://t.me/anom_invaders_bot?startapp=linkCode_${userId}`,
+    () => `https://t.me/anom_invaders_bot/battle?refer=${userId}`,
     [userId]
   );
 
